9676b7d10d2cb21757bfd881242ab46204039287
Lassiter Gregg <lassitergregg@gmail.com>
2018-05-04 00:12:50 -0500

    removed member access from member/family

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, :path => "", :default => { :format => :json }, :constraints => {:subdomain => "api"} do
     namespace :v1 do
-      # resources here
+      resource :member, :only => [] do #secured routes
+        resource :family
+      end
     end
   end
 end
