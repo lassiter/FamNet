c5507a87b53c409161db71ab45fbfd2439376b4a
Lassiter Gregg <lassitergregg@gmail.com>
2018-05-03 22:35:52 -0500

    edited Api -> API inflection and set planned namespace routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, :path => "", :default => { :format => :json }, :constraints => {:subdomain => "api"} do
+    namespace :v1 do
+      # resources here
+    end
+  end
 end
