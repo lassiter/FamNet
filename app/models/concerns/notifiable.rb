f0e8c93a7c31440cc22802ac2d067b2b3042a1c8
Lassiter Gregg <lassitergregg@gmail.com>
2018-06-16 17:41:54 -0500

    notify reactions simplified and some minor cleanup

--- app/models/concerns/notifiable.rb
+++ app/models/concerns/notifiable.rb
@@ -9,13 +9,18 @@ module Notifiable
   end
   
   def notify_members
-    # binding.pry
     @parent_klass = [Post, Event, Recipe].detect { |i| self.class == i }
-    @child_klass = [Comment, CommentReply, Interaction].detect { |i| self.class == i }
+    @child_klass = [Comment, CommentReply, Reaction].detect { |i| self.class == i }
+
+    # This is the object storing a string of the polymorphic klass for member lookup. (i.e. if the target is Comment then the attribute will be "commentable".)
     @target_attribute_polymorphic_klass = get_polymorphic_klass(self)
 
+    get_parent_and_target
     if self.class == @parent_klass # If it's a parent_klass, notify mentioned members.
       notify_mentioned_members if self.mentioned_members.any?
+    elsif @child_klass == Reaction
+      Notification.create(notifiable_type: @target.class.to_s, notifiable_id: @target.id, member_id: @parent.member_id)
+
     elsif self.class == @child_klass # If it's an child_klass, notify: mentioned members, parent_klass member (if not mentioned), and sibling child_klass.
       notify_mentioned_members if self.mentioned_members.any?
       notify_sibilings if @child_klass == Comment || @child_klass == CommentReply
@@ -23,7 +28,6 @@ module Notifiable
       if notify_mentioned_members.exclude?(@parent.member_id)
         Notification.create(notifiable_type: @target.class.to_s, notifiable_id: @target.id, member_id: @parent.member_id)
       end
-
     end
   end
 
@@ -34,29 +38,13 @@ module Notifiable
       sibilings_member_ids = self.class.where("#{@target_attribute_polymorphic_klass}_type": @parent.class.to_s, "#{@target_attribute_polymorphic_klass}_id": @parent.id).pluck(:member_id).uniq
     end
     
-    binding.pry
+    
     sibilings_member_ids.each do |sibilings_member_id|
       Notification.where(notifiable_type: @target.class.to_s, notifiable_id: @target.id, member_id: sibilings_member_id).first_or_create unless sibilings_member_id == @target.member_id
     end
   end
 
   def notify_mentioned_members
-    # This is the object storing a string of the polymorphic klass for member lookup. (i.e. if the target is Comment then the attribute will be "commentable".)
-    
-
-    # The target is the ActiveRecord that is Notifiable after it's been committed to the database.
-    # The parent is the Active Record of target and is used to notify the Ancestors.
-    @target = self
-    if @parent_klass
-      @parent = self
-    elsif @child_klass
-      if @child_klass == CommentReply
-        @parent = Comment.find(@target.comment_id)
-      else # Polymorphic Comment or Interaction
-        @parent = self["#{@target_attribute_polymorphic_klass.downcase}_type"].constantize.where(id:self["#{@target_attribute_polymorphic_klass.downcase}_id"]).first
-      end
-    end
-    # binding.pry
     # If there are mentions in the target then there will be an iteration over each mention 
     # and if the parent is not mentioned then they will recieve a notification.
     # If there are no mentions then the parent is notified.
@@ -68,34 +56,22 @@ module Notifiable
     end
     return @mentioned_members_id_array
 
-      # if @mentioned_members_id_array.include?(@parent.member_id) # Do any mentions include the parent?
-      #   # The parent has been mentioned and is included in the @mentioned_members_id_array.
-      #   @mentioned_members_id_array.each do |member_id|
-      #     Notification.create(notifiable_type: @target.class.to_s, notifiable_id: @target.id, member_id: member_id, mentioned: true)
-      #   end
-      # else # The parent has not been mentioned and is not in the @mentioned_members_id_array.
-      #   @mentioned_members_id_array.each do |member_id|
-      #     Notification.create(notifiable_type: @target.class.to_s, notifiable_id: @target.id, member_id: member_id, mentioned: true)
-      #   end
-      #   # The parent is notified with `mentioned: false` via DB Default.
-      #   Notification.create(notifiable_type: @target.class.to_s, notifiable_id: @target.id, member_id: @parent.member_id)
-      # end
-
-    # elsif ["Comment", "CommentReply", "Interaction"].any?(@target.class.to_s)
-    #   # This takes the klass from the get_polymorphic_klass method and finds the parent of the Comment, CommentReply, or Interaction.
-    #   Notification.create(notifiable_type: @target.class.to_s, notifiable_id: @target.id, member_id: @parent.member_id)
-    # end
-
-
-
-
   end
 
-
-
-
-
-
+  def get_parent_and_target
+      # The target is the ActiveRecord that is Notifiable after it's been committed to the database.
+      # The parent is the Active Record of target and is used to notify the Ancestors.
+      @target = self
+      if @parent_klass
+        @parent = self
+      elsif @child_klass
+        if @child_klass == CommentReply
+          @parent = Comment.find(@target.comment_id)
+        else # Polymorphic Comment or Reaction
+          @parent = self["#{@target_attribute_polymorphic_klass.downcase}_type"].constantize.where(id:self["#{@target_attribute_polymorphic_klass.downcase}_id"]).first
+        end
+      end
+  end
 
   # This method gets the polymorphic class attribute from the active record passed to it. 
   def get_polymorphic_klass(target)
