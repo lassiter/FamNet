3607909bc97c79c5d9542e34a7a755b75bb2f64b
Lassiter Gregg <lassitergregg@gmail.com>
2018-08-06 22:45:36 -0500

    family_config model and spec added

--- app/models/family.rb
+++ app/models/family.rb
@@ -8,4 +8,11 @@ class Family < ApplicationRecord
   has_many :invites
   
   validates :family_name, presence: true, format: { with: /\A\w[A-Za-z\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff\s'-]+\z/, message: "only allows letters" }, length: { minimum: 1 }
+  after_commit :spin_up_family_dependents
+
+  def spin_up_family_dependents
+    config = FamilyConfig.new(family_id: id)
+    config.save
+  end
+
 end
